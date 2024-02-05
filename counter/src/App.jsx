8c4ceb94456669f30642b11d28dfb2a79a0b6965
Raushan <raushan.kumar.cer20@itbhu.ac.in>
2024-02-05 16:34:05 +0530

    counter app done

---
+++ counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [count, setCount] = useState(10)
+
+//let counter = 1500;
+const incValue = () => {
+  if(count<20){
+    setCount(count+1);
+  }
+  else{
+    alert("Cannot increase more");
+  }
+  
+}
+const decValue = () => {
+  if(count>0){
+    setCount(count-1);
+  }
+  else{
+    alert("Cannot decrease more");
+  }
+  
+}
+  return (
+    <>
+     
+       
+    
+      <h1>This counter project is using react</h1>
+      <h2>Counter Value: {count}</h2>
+      <button onClick={incValue} >Increase</button>
+      <br />
+      <br />
+      <button onClick={decValue} >Decrease</button>
+      
+    </>
+  )
+}
+
+export default App
